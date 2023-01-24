9e0b512ad5744a355c0ca6612fe0749ff73ce34f
Dorthy Thielsen <dorthythielsen@gmail.com>
2023-01-24 18:30:07 -0500

    added notes and base function

---
+++ letter_case_permutation.js
@@ -0,0 +1,8 @@
+// takes in a string
+// transform each character to be upper or lowercase to create another string
+// return a list of all possible strings
+// return the output in any order
+
+function letterCasePermutation(s){
+
+}
