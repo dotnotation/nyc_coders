6641990fe134181f69dcce9ff9668fb738468928
Dorthy Thielsen <dorthythielsen@gmail.com>
2023-01-24 17:21:52 -0500

    added notes and base function

---
+++ subsets.js
@@ -0,0 +1,10 @@
+// takes in an integer array of UNIQUE elements
+// a list of unordered elements that are all unique
+// return subsets in any order
+// the full set is also a subset of itself
+// ex. num = [1, 2, 3]
+// returns [[],[1],[2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]]
+
+function subsets(num){
+
+}
