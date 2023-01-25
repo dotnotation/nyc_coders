db6d79f5558ecf5658bfa35f91c12ab6666af20e
Dorthy Thielsen <dorthythielsen@gmail.com>
2023-01-24 19:21:00 -0500

    finished code

--- letter_case_permutation.js
+++ letter_case_permutation.js
@@ -14,16 +14,21 @@ function helper(i, path, input, output){
     if (i === input.length){
         // strings are immutable 
         output.push(path)
+        return
     }
 
     // recursive case
     // if character include lower and upper
     if (isNaN(input[i])){
         // add lowercase
-
+        // taking lowercase element and adding it to the path
+        helper(i + 1, path + input[i].toLowerCase(), input, output)
         // add uppercase
-    }
+        helper(i + 1, path + input[i].toUpperCase(), input, output)
+    } else {
     // else add digit
+        helper(i + 1, path + input[i], input, output)
+    }
 
     return
 }
