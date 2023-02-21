10502b4ffa82bd6e372e5aabac64f04f4470127b
Dorthy Thielsen <dorthythielsen@gmail.com>
2023-02-21 17:53:10 -0500

    setting up stack class

---
+++ stack.js
@@ -0,0 +1,26 @@
+class Stack {
+    constructor(){
+        this.head = null
+        this.size = 0
+    }
+
+    push(val){
+
+    }
+
+    pop(){
+
+    }
+
+    peek(){
+
+    }
+
+    isEmpty(){
+
+    }
+
+    getSize(){
+        
+    }
+}
